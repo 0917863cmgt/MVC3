8fe1456bd5f8a04a759a4eeac65b9d02c3e83fc0
0917863cmgt <0917863cmgt@hr.nl>
2022-01-24 00:09:35 +0100

    changed comment component image source

--- resources/views/components/recipe/recipe-comment.blade.php
+++ resources/views/components/recipe/recipe-comment.blade.php
@@ -1,6 +1,6 @@
 @props(['recipe', 'comment', 'hasCommentLike', 'commentLikes'])
 <div class="comment-banner container">
-    <img class="comment-profile-image" src="{{$comment->user->profile_image}}">
+    <img class="comment-profile-image" src="{{ asset('storage/' . $comment->user->profile_image) }}">
     <p class="comment-username">{{$comment->user->username}}</p>
 </div>
 <div class="container">
