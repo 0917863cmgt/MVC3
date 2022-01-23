b35414b4fa1949bc792ff99bb32ea77a9ae76e13
0917863cmgt <0917863cmgt@hr.nl>
2022-01-23 23:41:46 +0100

    recipe card title string limited

--- resources/views/components/recipe/recipe-card.blade.php
+++ resources/views/components/recipe/recipe-card.blade.php
@@ -2,7 +2,7 @@
 <div class="col-3">
     <a href="/recipe/{{$recipe->slug}}" style="text-decoration: none;color:inherit; max-width: inherit">
         <img src="{{asset('storage/' . $recipe->image) ?: $recipe->image }}" width="300px" height="300px" style="width: 300px;height: 300px;object-fit: cover">
-        <h3>{{$recipe->title}}</h3>
+        <h3>{{Illuminate\Support\Str::limit($recipe->title,35,"...")}}</h3>
         <p>Amount of people: {{$recipe->amount_people}}</p>
         <p>Description:</p>
         <p>{{Illuminate\Support\Str::limit($recipe->description,200,"...")}}</p>
