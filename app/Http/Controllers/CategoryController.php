6d23910544adbb4a39f66c27de72f705f1f61bea
0917863cmgt <0917863cmgt@hr.nl>
2022-01-23 22:40:38 +0100

    Changed category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -46,10 +46,16 @@ class CategoryController extends Controller
     {
         $attributes = request()->validate([
             'is_parent' => 'required|integer|min:0|max:1',
-            'parent_id' => 'integer|min:1',
+            'parent_id' => 'integer',
             'name' => [Rule::unique('categories', 'name'), 'string', 'min:4' ],
             'slug' => [Rule::unique('categories', 'slug'), 'string', 'min:4' ]
         ]);
+
+        if(isset($attributes['parent_id']) & strlen($attributes['parent_id']) > 8){
+            $attributes['parent_id'] = bcrypt($attributes['parent_id']);
+        } else {
+            unset($attributes['parent_id']);
+        }
 
         Category::create($attributes);
 
