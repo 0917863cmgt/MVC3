785a0885070652745c4e601b659bae33477927ab
0917863cmgt <0917863cmgt@hr.nl>
2022-01-23 23:14:34 +0100

    Image changed again

--- resources/views/components/recipe/recipe-card.blade.php
+++ resources/views/components/recipe/recipe-card.blade.php
@@ -1,7 +1,7 @@
 @props(['recipe'])
 <div class="col-3">
     <a href="/recipe/{{$recipe->slug}}" style="text-decoration: none;color:inherit; max-width: inherit">
-        <img src="{{asset('storage/' . $recipe->image) ?: $recipe->image }}" width="300px" height="300px" style="width: 300px;height: 300px;">
+        <img src="{{asset('storage/' . $recipe->image) ?: $recipe->image }}" width="300px" height="300px" style="width: 300px;height: 300px;object-fit: cover">
         <h3>{{$recipe->title}}</h3>
         <p>Amount of people: {{$recipe->amount_people}}</p>
         <p>Description:</p>
